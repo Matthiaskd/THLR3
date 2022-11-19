577c8d9e81edd220a7761780f467b3565be6b74d
matthias-mehdi.kaddour <matthias-mehdi.kaddour@epita.fr>
2022-11-19 01:07:32 +0100

    final

--- tp4/thlr_4_code.py
+++ tp4/thlr_4_code.py
@@ -24,12 +24,12 @@ def convert_regex(enfa, origin, destination, regex):
         d=enfa.add_state()
 
         enfa.add_edge(origin,"",a)
-        enfa.add_edge(origin,"",b)
-        enfa.add_edge(c,"",destination)
+        enfa.add_edge(origin,"",c)
+        enfa.add_edge(b,"",destination)
         enfa.add_edge(d,"",destination)
 
-        convert_regex(enfa, a, c, regex.children[0])
-        convert_regex(enfa, b, d, regex.children[1])
+        convert_regex(enfa, a, b, regex.children[0])
+        convert_regex(enfa, c, d, regex.children[1])
 
     elif(op=="*"):
         a=enfa.add_state()
@@ -51,5 +51,34 @@ def to_enfa(regex):
     return NFA
 #[/Q3]
 
-aux=to_enfa(r2)
-aux.export("A")
+#[Q4]
+def get_epsilon_closure(enfa, origin):
+    visited=set()
+    incoming=set()
+    incoming.add(origin)
+    while(len(incoming)!=0):
+        aux=incoming.pop()
+        visited.add(aux)
+        e_forward=enfa.get_successors(aux,"")
+        for s in e_forward:
+            if not s in visited:
+                incoming.add(s)
+    return visited
+#[/Q4]
+
+#[Q5]
+def to_nfa(enfa):
+    alphabet=enfa.alphabet
+    states=enfa.all_states
+    for p in states:
+        e_fwd=get_epsilon_closure(enfa, p)
+        for q in e_fwd:
+            for a in alphabet:
+                succ=enfa.get_successors(q, a)
+                for q2 in succ:
+                    enfa.add_edge(p, a, q2)
+                    if(q2 in e_fwd and q2 in enfa.final_states):
+                        enfa.final_states.add(q)
+    enfa.alphabet.remove("")
+
+#[/Q5]
